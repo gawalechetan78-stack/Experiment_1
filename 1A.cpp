d5e97e57cbce3a90b5b362152e2d9a42063a567a
gawalechetan78-stack <gawalechetan78@gmail.com>
2025-09-29 13:17:59 +0530

    1A.cpp

---
+++ 1A.cpp
@@ -0,0 +1,18 @@
+// Name: Chetan Gawale 
+// PRN: 25070123507
+// Experiment 1(A)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+/* Output
+
+Hello, World!
+
+*/
